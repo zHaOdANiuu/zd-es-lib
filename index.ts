a7d37ac74cf180f5a80f0ee47ed1b538f7693dd2
zdn <136438302+zHaOdANiuu@users.noreply.github.com>
2024-07-02 14:01:39 +0800

    replace

--- index.ts
+++ index.ts
@@ -1,17 +1,14 @@
 const folder = new Folder('E:\\zd-es-lib\\src');
 const files = eachFolder(folder, /.ts$/);
 $.writeln(map(files, file => file.name).toString());
-
 const linkedList = new LinkedList();
 linkedList.append(1);
 linkedList.append(2);
 linkedList.append(3);
-
 linkedList.prepend(0);
 linkedList.insertAfter(3, 2);
 linkedList.append(4);
 linkedList.append(5);
 linkedList.delete(4);
-
 $.writeln(linkedList.toArray());
 $.writeln(globalThis);
