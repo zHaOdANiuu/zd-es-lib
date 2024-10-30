c0d811cc84c7a660fd4ad0a699b4c955ed2631ee
zdn <136438302+zHaOdANiuu@users.noreply.github.com>
2024-10-30 11:51:43 +0800

    更新导出成员

--- index.ts
+++ index.ts
@@ -26,13 +26,32 @@ export {
       activeCompItemEnviron
 } from './src/Ae';
 export {
+      HEX_CHARACTERS,
+      BASE_CHARACTERS,
+      BASE_CHARACTERS_TAB,
+      AE_MENU_COMMAND_ID,
+      FILE_SUFFIX,
+      FILE_WILDCARD,
+      MIN_AEPX,
+      MIN_IMAGE,
+      APP_PATH,
+      SUPPORT_FILES,
+      undef,
+      nativeSlice,
+      tempComp,
       timeToFrames,
       framesToTime,
-      isPanel,
-      isGroup,
-      isWindow,
-      isListItem,
-      isTreeViewNode,
+      isReadonly,
+      isReadwrite,
+      isNull,
+      isUndefined,
+      isBoolean,
+      isNumber,
+      isString,
+      isFunction,
+      isArray,
+      isFile,
+      isFolder,
       isProperty,
       isPropertyGroup,
       isCompItem,
@@ -42,26 +61,22 @@ export {
       isCameraLayer,
       isLightLayer,
       isSolidSource,
-      isFolder,
-      isFile
-} from './src/base/const';
-export {
-      ArrayPrototype,
-      ObjectPrototype,
-      nativeSlice,
+      isPanel,
+      isGroup,
+      isWindow,
+      isTreeView,
+      isStaticText,
+      isEditText,
+      isButton,
+      isCheckbox,
+      isSlider,
+      isCustom,
+      isListItem,
+      isTreeViewNode,
       hasOwn,
       getTag,
       isEmptyArray,
       isEmptyObject,
-      undef,
-      HEX_CHARACTERS,
-      BASE_CHARACTERS,
-      BASE_CHARACTERS_TAB,
-      AE_MENU_COMMAND_ID,
-      FILE_SUFFIX,
-      FILE_WILDCARD,
-      MIN_AEPX,
-      MIN_IMAGE,
       escapeRegExp,
       removeQuotes,
       removeSpaces,
@@ -73,12 +88,36 @@ export {
       degreesToRadians,
       radiansToDegrees,
       dihedralAngle,
-      iterateeCode,
-      clamp
-} from './src/global/const';
-export { forEach, map, filter, reduce, create, keys, bind, trim, trimEnd, trimStart } from './src/lib/es5';
-export { find, findIndex, fill, from, assign, repeat } from './src/lib/es6';
-export { entries, padStart, padEnd } from './src/lib/es8';
+      clamp,
+      iterateeCode
+} from './src/base/const';
+export {
+      indexOf,
+      lastIndexOf,
+      forEach,
+      map,
+      filter,
+      reduce,
+      create,
+      keys,
+      bind,
+      trim,
+      trimEnd,
+      trimStart
+} from './src/lib/es5';
+export {
+      find,
+      findIndex,
+      fill,
+      from,
+      assign,
+      repeat
+} from './src/lib/es6';
+export {
+      entries,
+      padStart,
+      padEnd
+} from './src/lib/es8';
 export { default as $error } from './src/$error';
 export { default as $eval } from './src/$eval';
 export { default as addProperties } from './src/addProperties';
@@ -87,18 +126,15 @@ export { default as binarySearch } from './src/binarySearch';
 export { default as breadthTraversesFolders } from './src/breadthTraversesFolders';
 export { default as cubicBezier } from './src/cubicBezier';
 export { default as cubicBezierToArray } from './src/cubicBezierToArray';
+export { default as debounce } from './src/debounce';
 export { default as deeplyTraverseFolder } from './src/deeplyTraverseFolder';
 export { default as duffDevice } from './src/duffDevice';
 export { default as duplicateLayer } from './src/duplicateLayer';
 export { default as exe } from './src/exe';
 export { default as findProperty } from './src/findProperty';
-export { default as getClipboard } from './src/getClipboard';
 export { default as getLayerType } from './src/getLayerType';
 export { default as imageToArray } from './src/imageToArray';
 export { default as importCompItem } from './src/importCompItem';
-export { default as lrcToTextLayer } from './src/lrcToTextLayer';
-export { default as openFileLocation } from './src/openFileLocation';
-export { default as pcInfo } from './src/pcInfo';
 export { default as randomNumberArray } from './src/randomNumberArray';
 export { default as readAEPrefLabelData } from './src/readAEPrefLabelData';
 export { default as readFile } from './src/readFile';
@@ -117,8 +153,9 @@ export { default as writeFile } from './src/writeFile';
 export { default as writeJSON } from './src/writeJSON';
 export { default as writeXML } from './src/writeXML';
 export { default as arrayEqualizeLengths } from './src/base/arrayEqualizeLengths';
+export { default as findMaxValue } from './src/base/findMaxValue';
+export { default as findMinValue } from './src/base/findMinValue';
 export { default as forOwn } from './src/base/forOwn';
-export { default as fullData } from './src/base/fullData';
 export { default as hexToDec } from './src/base/hexToDec';
 export { default as hexToString } from './src/base/hexToString';
 export { default as intToString } from './src/base/intToString';
@@ -133,14 +170,18 @@ export { default as stringToInt8 } from './src/base/stringToInt8';
 export { default as stringToInt32 } from './src/base/stringToTnt';
 export { default as traverseHierarchy } from './src/base/traverseHierarchy';
 export { default as EffectExtends } from './src/class/EffectExtends';
+export { default as PropertyExtends } from './src/class/PropertyExtends';
 export { default as ScriptUIParser } from './src/class/ScriptUIParser';
 export { default as TreeViewExtends } from './src/class/TreeViewExtends';
 export { default as copyToClipboard } from './src/cmd/copyToClipboard';
+export { default as getClipboard } from './src/cmd/getClipboard';
+export { default as getPcInfo } from './src/cmd/getPcInfo';
+export { default as openFileLocation } from './src/cmd/openFileLocation';
 export { default as screenShotImport } from './src/cmd/screenShotImport';
 export { default as systemQuit } from './src/cmd/systemQuit';
 export { default as addElementDown } from './src/event/addElementDown';
 export { default as addElementToLeftAndRight } from './src/event/addElementToLeftAndRight';
-export { default as debounce } from './src/event/debounce';
+export { default as drawPath } from './src/event/drawPath';
 export { default as enterEvent } from './src/event/enterEvent';
 export { default as mouseMoveElement } from './src/event/mouseMoveElement';
 export { default as mouseMoveEnviron } from './src/event/mouseMoveEnviron';
@@ -161,4 +202,5 @@ export { default as Queue } from './src/global/Queue';
 export { default as Stack } from './src/global/Stack';
 export { default as $$$ColorPicker } from './src/script/$$$ColorPicker';
 export { default as $$$LoadWindow } from './src/script/$$$LoadWindow';
-export { default as $$$openImageInPhotoshop } from './src/script/$$$openImageInPhotoshop';
+export { default as $$$LrcToTextLayer } from './src/script/$$$LrcToTextLayer';
+export { default as $$$OpenImageInPhotoshop } from './src/script/$$$OpenImageInPhotoshop';
